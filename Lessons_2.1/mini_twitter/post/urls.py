0d2cfba0543b7d3bc54b85a221fdbd0f4b80ad18
Sasha Vasiliev <Alexandr@MacBookAlex.local>
2023-12-02 17:42:44 +0200

    Edit urls and views

--- Lessons_2.1/mini_twitter/post/urls.py
+++ Lessons_2.1/mini_twitter/post/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
 
     path('', views.list_post, name='list_post'),
-    path('<str:username>/', views.list_post, name='post_list_filtered'),
+    path('post/<str:username>/', views.list_postf, name='post_list_filtered'),
     path('comment/', views.list_comments, name='list_comment')
 ]
--- Lessons_2.1/mini_twitter/post/views.py
+++ Lessons_2.1/mini_twitter/post/views.py
@@ -1,16 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from users.models import Users
 
 
-def list_post(request, username=None):
+def list_post(request):
+    post = Post.objects.all()
+    context = {'post': post}
+    return render(request, 'post/post_list.html', context)
+
+
+def list_postf(request, username=None):
     if username:
-        user = Users.objects.get(username=username)
+        user = get_object_or_404(Users, username=username)
         post = Post.objects.filter(users=user)
     else:
         post = Post.objects.all()
 
-    context = {'post': post, 'username': user}
+    context = {'post': post, 'username': username}
     return render(request, 'post/post_list.html', context)
 
 
